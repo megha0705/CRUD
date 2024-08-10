cfc07d7d122563dc8e824dbf996d9066e63b4f5a
Megha <chakrabortyreshmi077@gmail.com>
2024-08-10 11:28:16 +0530

    Employee table

--- jdbc/src/Employee.java
+++ jdbc/src/Employee.java
@@ -1,14 +1,26 @@
 public class Employee {
-    private int emp_id;
+    private int id;
     private String name;
     private String email;
     private  int salary;
-    public emp(int emp_id, String name , String email , int salary){
-        this.emp_id = emp_id;
+    public Employee(int id, String name , String email , int salary){
+        this.id = id;
         this.name = name;
         this.email =email;
         this.salary = salary;
     }
 
+    public int getId(){
+        return id;
+    }
+    public String  getEmail(){
+        return email;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getSalary(){
+        return salary;
+    }
 
 }

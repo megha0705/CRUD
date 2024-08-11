e66626cecdd81fc06654611029642d23a8282ac9
Megha <chakrabortyreshmi077@gmail.com>
2024-08-11 10:43:19 +0530

    insert query string

--- jdbc/src/Query.java
+++ jdbc/src/Query.java
@@ -1,3 +1,5 @@
-Class Query{
+ public class Query{
+   static String select = "SELECT * FROM  Employee";
+   static String insert = "INSERT INTO Employee(id , name , email , salary) VALUES (? , ? , ? ,?)"; 
     
-}
+ }

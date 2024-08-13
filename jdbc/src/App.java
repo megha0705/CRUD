a416701ffe8a92d7e4e8534e5afe6c5409bba60d
Megha <chakrabortyreshmi077@gmail.com>
2024-08-13 11:02:10 +0530

    printing table in the arraylist form

--- jdbc/src/App.java
+++ jdbc/src/App.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+
 import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -8,8 +11,10 @@ public class App {
         boolean n = true;
         while(n){
             switch (choice) {
-                case 1:  EmployeeDao.selectEmployee(); 
-                
+                case 1: ArrayList<Employee> l = EmployeeDao.selectEmployee();
+                for(Employee m : l){
+                    System.out.println(m);
+                }
                 break;
                 case 2: System.out.println("are u sure you want to delete the table? Y/N");
                 String s1 = sc.nextLine();
@@ -19,14 +24,20 @@ public class App {
                 break;
                 case 3 : System.out.println("id = ? || name = ? || email = ? || salary = ?");
                 int id =  sc.nextInt();
+                sc.nextLine(); //To get rid of the newline character "/n"
                 String name = sc.nextLine();
                 String email = sc.nextLine();
                 int salary = sc.nextInt();
                 Employee emp = new Employee(id , name , email, salary);
                 EmployeeDao.createEmployee(emp); 
                 break;
-                case 4: System.out.println("column you want to upate "); // due
-                break;
+                /*case 4: System.out.println("what do u wanna update? 1 - id , 2 - name , 3 - email, 4 - salary. please provide the updated value too"); 
+                        id = sc.nextInt();
+                       sc.nextLine();
+                        name = sc.nextLine();
+                       Employee emp = new Employee(id , name);
+
+                break;*/
                 case 5 :  EmployeeDao.descEmployee(); 
                 break;
                 case 6: n = false;

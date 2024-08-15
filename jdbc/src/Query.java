371b2ff67020c6a2820dbad9c1aeeccdbf48e1a2
Megha <chakrabortyreshmi077@gmail.com>
2024-08-15 11:52:19 +0530

    adjusted update query

--- jdbc/src/Query.java
+++ jdbc/src/Query.java
@@ -1,5 +1,8 @@
  public class Query{
    static String select = "SELECT * FROM  Employee";
    static String insert = "INSERT INTO Employee(id , name , email , salary) VALUES (? , ? , ? ,?)"; 
-    
+   static String desc = "DESC  Employee" ;
+   static String delete =  "DROP TABLE Employee";
+   static String update = "UPDATE Employee SET " + columnType  + " = ? WHERE "+ conditionColumn +  " = ?";
+   
  }

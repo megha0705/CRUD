96ea08018fe237fa9c8669dd6fddd7c056f95c6e
Megha <chakrabortyreshmi077@gmail.com>
2024-08-16 21:19:10 +0530

    calling functions from App.java

--- jdbc/src/Query.java
+++ jdbc/src/Query.java
@@ -1,8 +1,9 @@
- public class Query{
+ public  class Query{
+   App a = new App();
    static String select = "SELECT * FROM  Employee";
    static String insert = "INSERT INTO Employee(id , name , email , salary) VALUES (? , ? , ? ,?)"; 
    static String desc = "DESC  Employee" ;
    static String delete =  "DROP TABLE Employee";
-   static String update = "UPDATE Employee SET " + columnType  + " = ? WHERE "+ conditionColumn +  " = ?";
+   static String update = "UPDATE Employee SET " + a.columnType()  + " = ? WHERE "+ a.conditionColumn()  +  " = ?";
    
  }
